4068337d9a6610561e13ca7a4356f72e8c62caf0
p_pzqfang <p_pzqfang@tencent.com>
2021-11-28 20:08:59 +0800

    fork

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: '2035tw',
-      exec_mode: 'cluster',
+      exec_mode: 'fork',
       instances: 1, // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
