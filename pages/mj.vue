2a15b0094bee07db77f92a39f579bf7c5f40d1f3
fangdown <409951398@qq.com>
2021-12-19 22:15:25 +0800

    增加面经

---
+++ pages/mj.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <a-row v-for="item in list" :key="item.id" class="row">
+      <a-col class="col">
+        <a :href="item.value" target="_blank">{{ item.name }}</a>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return { list: [] };
+  },
+  async asyncData({ $axios }) {
+    const res = await $axios.$get("https://api.git123.cn/git123/link/list-mj");
+    const { data = {} } = res || {};
+    return { list: data.rows || [] };
+  },
+};
+</script>
+<style scoped>
+.row {
+  padding: 10px;
+}
+</style>
